233bec61c4ece6bac2bda947f3ca05028de63a94
Mario Ernesto Carranza <mario.carranza996@gmail.com>
2023-06-07 16:29:52 -0600

    first commit

---
+++ src/layout/AppMenu.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { ref } from 'vue';
+
+import AppMenuItem from './AppMenuItem.vue';
+
+const model = ref([
+    {
+        label: 'Home',
+        items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/admin/dashboard' }]
+    },
+    {
+        label: 'Administración',
+        items: [
+			{
+                label: 'Usuarios',
+                icon: 'pi pi-fw pi-users',
+                to: '/admin/usuarios'
+            },
+			{
+                label: 'Libros',
+                icon: 'pi pi-fw pi-book',
+                to: '/admin/libros'
+            },
+			{
+                label: 'Multas',
+                icon: 'pi pi-fw pi-wallet',
+                to: '/admin/multas'
+            },
+			{
+                label: 'Prestamos',
+                icon: 'pi pi-fw pi-bookmark',
+                to: '/admin/prestamos'
+            },
+        ]
+    },
+    {
+        label: 'Utilities',
+        items: [
+            { label: 'PrimeIcons', icon: 'pi pi-fw pi-prime', to: '/utilities/icons' },
+            { label: 'PrimeFlex', icon: 'pi pi-fw pi-desktop', url: 'https://www.primefaces.org/primeflex/', target: '_blank' }
+        ]
+    },
+    {
+        label: 'Pages',
+        icon: 'pi pi-fw pi-briefcase',
+        to: '/pages',
+        items: [
+            {
+                label: 'Landing',
+                icon: 'pi pi-fw pi-globe',
+                to: '/landing'
+            },
+            {
+                label: 'Auth',
+                icon: 'pi pi-fw pi-user',
+                items: [
+                    {
+                        label: 'Login',
+                        icon: 'pi pi-fw pi-sign-in',
+                        to: '/auth/login'
+                    },
+                ]
+            },
+            {
+                label: 'Crud',
+                icon: 'pi pi-fw pi-pencil',
+                to: '/pages/crud'
+            },
+            {
+                label: 'Empty',
+                icon: 'pi pi-fw pi-circle-off',
+                to: '/pages/empty'
+            }
+        ]
+    },
+]);
+</script>
+
+<template>
+    <ul class="layout-menu">
+        <template v-for="(item, i) in model" :key="item">
+            <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
+            <li v-if="item.separator" class="menu-separator"></li>
+        </template>
+    </ul>
+</template>
+
+<style lang="scss" scoped></style>
